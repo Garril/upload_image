cbb7252968c71805a01efc0c89bf693d541b8d6c
Garril <zev20010120@163.com>
2023-04-14 01:25:09 +0800

    first commit

---
+++ pages/index/config-example.js
@@ -0,0 +1,14 @@
+const host = 'https://forupxxxx.oss-cn-guangzhou.aliyuncs.com';
+const ossAccessKeyId = 'LTAI5txxxxxxxxxxxx';
+const signature = 'UdHrMGRvGgacxxxxxxxxxxxxxxxxxx';
+const policy = 'eyJleHBpcmF0aW9uIjoiMjAxxxxxxxxxxxxxxcmFuZ2UiLDAsMTA0ODU3NjBdXX0=';
+const key = 'imgs/test.png'; // 目标路径
+// const securityToken = '<x-oss-security-token>';
+// const filePath = '/pages/assets/test1.png'; // 待上传文件的文件路径。
+export {
+	host,
+	ossAccessKeyId,
+	signature,
+	policy,
+	key
+}
